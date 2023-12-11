72908ca766e674630b30cf8541140b959dcbc36e
Sosea1 <kospryshin@gmail.com>
2023-12-12 01:42:09 +0600

    1

--- src/components/MainMonitoring/Frame20/Frame20.tsx
+++ src/components/MainMonitoring/Frame20/Frame20.tsx
@@ -5,7 +5,10 @@ import resets from '../_resets.module.css';
 import { _Property1Variant4 } from './_Property1Variant4/_Property1Variant4';
 import classes from './Frame20.module.css';
 import { PingButton_Property1Default } from './PingButton_Property1Default/PingButton_Property1Default';
-
+import { IoMdAddCircle } from "react-icons/io";
+import { TiDelete } from "react-icons/ti";
+import { FaRegEdit } from "react-icons/fa";
+import { RiDeleteBin6Line } from "react-icons/ri";
 interface Props {
   className?: string;
 }
@@ -15,7 +18,7 @@ export const Frame20: FC<Props> = memo(function Frame20(props = {}) {
     <div className={`${resets.storybrainResets} ${classes.root}`}>
       <div className={classes.globalConfig}>Global Config</div>
       <div className={classes.line1}></div>
-      <div className={classes.line2}></div>
+      
       <div className={classes.line3}></div>
       <div className={classes.frame16}>
         <div className={classes.sNMP}>SNMP</div>
@@ -23,14 +26,14 @@ export const Frame20: FC<Props> = memo(function Frame20(props = {}) {
         <_Property1Variant4
           className={classes.unnamed}
           text={{
-            _1921681: <div className={classes._1921681}>64</div>,
+            _1921681: <input className={classes._1921681}/>,
           }}
         />
         <div className={classes.localEngineID}>Local Engine ID</div>
         <_Property1Variant4
           className={classes.unnamed2}
           text={{
-            _1921681: <div className={classes._19216812}>10</div>,
+            _1921681: <input className={classes._19216812}/>,
           }}
         />
         <PingButton_Property1Default
@@ -39,42 +42,83 @@ export const Frame20: FC<Props> = memo(function Frame20(props = {}) {
             ping: <div className={classes.ping}>Apply</div>,
           }}
         />
-        <div className={classes.frame17}></div>
+        <div className={classes.frame17}>
+        
+        </div>
+        <input type="checkbox" className={classes.input2}/>
+        <input type="checkbox" className={classes.input3}/>
+        <input type="checkbox" className={classes.input4}/>
+        <input type="checkbox" className={classes.input5}/>
         <div className={classes.rectangle4}></div>
         <div className={classes.rectangle5}></div>
         <div className={classes.total4}>Total: 4</div>
         <div className={classes.delete}>Delete</div>
+        <TiDelete className={classes.delete1}/>
         <div className={classes.frame21}>
+        <input type="checkbox" className={classes.input4}/>
+        
           <div className={classes._2}>2</div>
           <div className={classes.viewDefault}>ViewDefault</div>
           <div className={classes.exclude}>Exclude</div>
           <div className={classes._13616315}>1.3.6.1.6.3.15</div>
+          <FaRegEdit style={{left: '280px',
+  top: '0px',
+  position: 'relative'}}/>
+          <RiDeleteBin6Line style={{left: '230px',
+  top: '0px',
+  position: 'relative'}}/>
         </div>
         <div className={classes.frame212}>
+        <input type="checkbox" className={classes.input3}/>
+        
           <div className={classes._1}>1</div>
           <div className={classes.viewDefault2}>ViewDefault</div>
           <div className={classes.include}>Include</div>
           <div className={classes._12}>1</div>
+          <FaRegEdit style={{left: '280px',
+  top: '0px',
+  position: 'relative'}}/>
+          <RiDeleteBin6Line style={{left: '230px',
+  top: '0px',
+  position: 'relative'}}/>
         </div>
         <div className={classes.frame22}>
+        <input type="checkbox" className={classes.input5}/>
+        
           <div className={classes._3}>3</div>
           <div className={classes.viewDefault3}>ViewDefault</div>
           <div className={classes.exclude2}>Exclude</div>
           <div className={classes._13616316}>1.3.6.1.6.3.16</div>
+          <FaRegEdit style={{left: '280px',
+  top: '0px',
+  position: 'relative'}}/>
+          <RiDeleteBin6Line style={{left: '230px',
+  top: '0px',
+  position: 'relative'}}/>
         </div>
         <div className={classes.frame213}>
           <div className={classes.index}>Index</div>
+        <input type="checkbox" className={classes.input2}/>
+
           <div className={classes.viewName}>View Name</div>
           <div className={classes.viewType}>View Type</div>
           <div className={classes.mIBObjectID}>MIB Object ID</div>
           <div className={classes.pingResult}>Ping Result</div>
         </div>
         <div className={classes.frame23}>
+        <input type="checkbox" className={classes.input6}/>
           <div className={classes._4}>4</div>
           <div className={classes.viewDefault4}>ViewDefault</div>
           <div className={classes.exclude3}>Exclude</div>
           <div className={classes._13616218}>1.3.6.1.6.2.18</div>
+          <FaRegEdit style={{left: '280px',
+  top: '0px',
+  position: 'relative'}}/>
+          <RiDeleteBin6Line style={{left: '230px',
+  top: '0px',
+  position: 'relative'}}/>
         </div>
+        <input type="checkbox" className={classes.input1}/>
         <div className={classes.enable}>Enable</div>
         <PingButton_Property1Default
           className={classes.pingButton2}
@@ -86,6 +130,7 @@ export const Frame20: FC<Props> = memo(function Frame20(props = {}) {
         <div className={classes._1064Hex}>(10-64 Hex)</div>
         <div className={classes.NullOr1064Hex}>(Null or 10-64 Hex)</div>
         <div className={classes.add}>Add</div>
+        <IoMdAddCircle className={classes.add1} />
       </div>
     </div>
   );
--- src/components/MainMonitoring/SNMP_Property1Default/SNMP_Property1Default.tsx
+++ src/components/MainMonitoring/SNMP_Property1Default/SNMP_Property1Default.tsx
@@ -9,6 +9,7 @@ import ReactDomServer from 'react-dom/server'
 import { createRoot } from 'react-dom/client';
 import { _Property1Variant4 } from '../../Diagnosis/_Property1Variant4/_Property1Variant4';
 import { PingButton_Property1Default } from '../../Diagnosis/PingButton_Property1Default/PingButton_Property1Default';
+import { Frame20 } from '../Frame20/Frame20';
 
 interface Props {
   className?: string;
@@ -22,66 +23,7 @@ export const SNMP_Property1Default: FC<Props> = memo(function SNMP_Property1Defa
   
 const ComponentOne =(): React.ReactNode  => {
 
-  return <div className={classes.frame19}>
-  <div className={classes.ping}>Ping </div>
-  <div className={classes.line1}></div>
-  <div className={classes.frame16}>
-    <div className={classes.destinationIP}>Destination IP: </div>
-    <_Property1Variant4 className={classes.unnamed} />
-    <div className={classes.dataSize}>Data Size: </div>
-    <_Property1Variant4
-      className={classes.unnamed2}
-      text={{
-        _1921681: <div className={classes._1921681}>64</div>,
-      }}
-    />
-    <div className={classes.interval}>Interval: </div>
-    <_Property1Variant4
-      className={classes.unnamed3}
-      text={{
-        _1921681: <div className={classes._19216812}>1000</div>,
-      }}
-    />
-    <div className={classes.pingTimes}>Ping Times: </div>
-    <_Property1Variant4
-      className={classes.unnamed4}
-      text={{
-        _1921681: <div className={classes._19216813}>10</div>,
-      }}
-    />
-    <PingButton_Property1Default className={classes.pingButton} />
-    <div className={classes.frame17}>
-      <div className={classes.frame18}>
-        <div className={classes.pinging1921681With64BytesOfDat}>
-          <div className={classes.textBlock}>Pinging 192.168.0.1 with 64 bytes of data:</div>
-          <div className={classes.textBlock2}>Reply from 3001::1234 : bytes=64 time=16ms</div>
-          <div className={classes.textBlock3}>Reply from 192.168.0.146 : bytes=64 time=16ms TTL=64</div>
-          <div className={classes.textBlock4}>Destination Host Unreachable!</div>
-          <div className={classes.textBlock5}>Request Timeout.</div>
-        </div>
-      </div>
-      <div className={classes.packetsSent4Received4LossLoss}>
-        <p className={classes.labelWrapper}>
-          <span className={classes.label}>Packets: </span>
-          <span className={classes.label2}>Sent=4, Received=4, Loss=0 (0%Loss)</span>
-        </p>
-      </div>
-      <div className={classes.maximum2msMinimum1msAverage1ms}>
-        <p className={classes.labelWrapper2}>
-          <span className={classes.label3}>Maximum</span>
-          <span className={classes.label4}>=2ms, Minimum=1ms, Average=1ms</span>
-        </p>
-        <div className={classes.textBlock6}>
-          <p></p>
-        </div>
-      </div>
-    </div>
-    <div className={classes.rectangle4}></div>
-    <div className={classes.pingResult}>Ping Result</div>
-    <div className={classes.oK}>OK</div>
-  </div>
-</div>;
-  
+  return <Frame20/>
  };
  
  
