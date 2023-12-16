945e57a8dda8a29ca2c834d20fef9e9e1f7b7f8b
tuleshochka <sizyno.min@gmail.com>
2023-12-17 02:14:01 +0600

    ~

--- src/components/Diagnosis/Diagnosis.tsx
+++ src/components/Diagnosis/Diagnosis.tsx
@@ -1,5 +1,5 @@
-import { memo } from 'react';
-import type { FC } from 'react';
+import { memo, useEffect, useState } from 'react';
+import type { FC, ReactElement } from 'react';
 
 import resets from '../_resets.module.css';
 import { _Property1Variant4 } from './_Property1Variant4/_Property1Variant4';
@@ -15,12 +15,44 @@ import { QoS_Property1Default } from './QoS_Property1Default/QoS_Property1Defaul
 import { Security_Property1Default } from './Security_Property1Default/Security_Property1Default';
 import { SideMenu_Property1Diagnosis } from './SideMenu_Property1Diagnosis/SideMenu_Property1Diagnosis';
 import { System_Property1Default } from './System_Property1Default/System_Property1Default';
+import ReactDomServer from 'react-dom/server'
+import { useDispatch } from 'react-redux';
+import { update } from '../../main';
 
 interface Props {
   className?: string;
 }
 /* @figmaId 66:981 */
 export const Diagnosis: FC<Props> = memo(function Diagnosis(props = {}) {
+
+  const ComponentOne = (): React.ReactNode =>  {
+ 
+    return <>1</>;
+   
+   };
+
+    const dispatch = useDispatch();
+
+    const handleUpdate = (key: string) => {
+      dispatch(update(key));
+    };
+
+    const [currentComponent, setCurrentComponent] = useState<React.ReactNode>(null);
+
+    useEffect(() => {
+      setCurrentComponent(currentComponent)
+      var x = document.getElementById('diagnosis_frame') as HTMLElement;
+      x.innerHTML = ReactDomServer.renderToString(currentComponent as ReactElement);
+    },[currentComponent])
+ 
+
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+      setIsLoaded(true);
+      handleUpdate('NetworkD') 
+      setCurrentComponent(ComponentOne)
+   }, []);
+
   return (
     <div className={`${resets.storybrainResets} ${classes.root}`}>
       <div id='diagnosis_frame' className={classes.frame14} ></div>
--- src/components/Security/Security.tsx
+++ src/components/Security/Security.tsx
@@ -1,5 +1,5 @@
-import { memo } from 'react';
-import type { FC } from 'react';
+import { memo, useEffect, useState } from 'react';
+import type { FC, ReactElement } from 'react';
 
 import resets from '../_resets.module.css';
 import { Diagnose_Property1Default } from './Diagnose_Property1Default/Diagnose_Property1Default';
@@ -12,12 +12,43 @@ import classes from './Security.module.css';
 import { Security_Property1Variant3 } from './Security_Property1Variant3/Security_Property1Variant3';
 import { SideMenu_Property1Security } from './SideMenu_Property1Security/SideMenu_Property1Security';
 import { System_Property1Default } from './System_Property1Default/System_Property1Default';
+import ReactDomServer from 'react-dom/server'
+import { useDispatch } from 'react-redux';
+import { update } from '../../main';
 
 interface Props {
   className?: string;
 }
 /* @figmaId 66:845 */
 export const Security: FC<Props> = memo(function Security(props = {}) {
+  
+  const ComponentOne =(): React.ReactNode  => {
+
+    return <>1</>
+   };
+
+    const dispatch = useDispatch();
+
+    const handleUpdate = (key: string) => {
+      dispatch(update(key));
+    };
+
+    const [currentComponent, setCurrentComponent] = useState<React.ReactNode>(null);
+
+    useEffect(() => {
+      setCurrentComponent(currentComponent)
+      var x = document.getElementById('security_frame') as HTMLElement;
+      x.innerHTML = ReactDomServer.renderToString(currentComponent as ReactElement);
+    },[currentComponent])
+ 
+
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+      setIsLoaded(true);
+      handleUpdate('AccessSecurity') 
+      setCurrentComponent(ComponentOne)
+   }, []);
+
   return (
     <div className={`${resets.storybrainResets} ${classes.root}`}>
       <div id='security_frame' className={classes.frame14} ></div>
