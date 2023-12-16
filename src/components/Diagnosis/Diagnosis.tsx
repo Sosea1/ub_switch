9f85dbb7eee216c2051ee62c7830700e31972e76
tuleshochka <sizyno.min@gmail.com>
2023-12-16 23:27:05 +0600

    ~

--- src/components/Diagnosis/Diagnosis.tsx
+++ src/components/Diagnosis/Diagnosis.tsx
@@ -23,6 +23,7 @@ interface Props {
 export const Diagnosis: FC<Props> = memo(function Diagnosis(props = {}) {
   return (
     <div className={`${resets.storybrainResets} ${classes.root}`}>
+      <div id='diagnosis_frame' className={classes.frame14} ></div>
       <div className={classes.mainMenu}>
         <L2functions_Property1Default className={classes.l2functions} />
         <L3functions_Property1Default className={classes.l3functions} />
@@ -41,71 +42,7 @@ export const Diagnosis: FC<Props> = memo(function Diagnosis(props = {}) {
           <Extercom_logo11Icon className={classes.icon} />
         </div>
       </div>
-      <SideMenu_Property1Diagnosis
-        className={classes.sideMenu}
-        swap={{
-          networkDiagnostics: <NetworkDiagnostics_Property1Va className={classes.networkDiagnostics} />,
-        }}
-      />
-      <div className={classes.frame19}>
-        <div className={classes.ping}>Ping </div>
-        <div className={classes.line1}></div>
-        <div className={classes.frame16}>
-          <div className={classes.destinationIP}>Destination IP: </div>
-          <_Property1Variant4 className={classes.unnamed} />
-          <div className={classes.dataSize}>Data Size: </div>
-          <_Property1Variant4
-            className={classes.unnamed2}
-            text={{
-              _1921681: <div className={classes._1921681}>64</div>,
-            }}
-          />
-          <div className={classes.interval}>Interval: </div>
-          <_Property1Variant4
-            className={classes.unnamed3}
-            text={{
-              _1921681: <div className={classes._19216812}>1000</div>,
-            }}
-          />
-          <div className={classes.pingTimes}>Ping Times: </div>
-          <_Property1Variant4
-            className={classes.unnamed4}
-            text={{
-              _1921681: <div className={classes._19216813}>10</div>,
-            }}
-          />
-          <PingButton_Property1Default className={classes.pingButton} />
-          <div className={classes.frame17}>
-            <div className={classes.frame18}>
-              <div className={classes.pinging1921681With64BytesOfDat}>
-                <div className={classes.textBlock}>Pinging 192.168.0.1 with 64 bytes of data:</div>
-                <div className={classes.textBlock2}>Reply from 3001::1234 : bytes=64 time=16ms</div>
-                <div className={classes.textBlock3}>Reply from 192.168.0.146 : bytes=64 time=16ms TTL=64</div>
-                <div className={classes.textBlock4}>Destination Host Unreachable!</div>
-                <div className={classes.textBlock5}>Request Timeout.</div>
-              </div>
-            </div>
-            <div className={classes.packetsSent4Received4LossLoss}>
-              <p className={classes.labelWrapper}>
-                <span className={classes.label}>Packets: </span>
-                <span className={classes.label2}>Sent=4, Received=4, Loss=0 (0%Loss)</span>
-              </p>
-            </div>
-            <div className={classes.maximum2msMinimum1msAverage1ms}>
-              <p className={classes.labelWrapper2}>
-                <span className={classes.label3}>Maximum</span>
-                <span className={classes.label4}>=2ms, Minimum=1ms, Average=1ms</span>
-              </p>
-              <div className={classes.textBlock6}>
-                <p></p>
-              </div>
-            </div>
-          </div>
-          <div className={classes.rectangle4}></div>
-          <div className={classes.pingResult}>Ping Result</div>
-          <div className={classes.oK}>OK</div>
-        </div>
-      </div>
+      <SideMenu_Property1Diagnosis className={classes.sideMenu}/>
     </div>
   );
 });
--- src/components/Diagnosis/NetworkDiagnostics_Property1De/NetworkDiagnostics_Property1De.tsx
+++ src/components/Diagnosis/NetworkDiagnostics_Property1De/NetworkDiagnostics_Property1De.tsx
@@ -1,9 +1,13 @@
-import { memo } from 'react';
-import type { FC } from 'react';
+import { memo, useEffect, useState } from 'react';
+import type { FC, ReactElement } from 'react';
 
 import resets from '../../_resets.module.css';
 import classes from './NetworkDiagnostics_Property1De.module.css';
 import { Vector3Icon } from './Vector3Icon.js';
+import { useDispatch, useSelector } from 'react-redux';
+import { CounterState, update } from '../../../main';
+import ReactDomServer from 'react-dom/server'
+import { SlArrowRight } from 'react-icons/sl';
 
 interface Props {
   className?: string;
@@ -13,12 +17,88 @@ interface Props {
 }
 /* @figmaId 260:1341 */
 export const NetworkDiagnostics_Property1De: FC<Props> = memo(function NetworkDiagnostics_Property1De(props = {}) {
+  const ComponentOne =(): React.ReactNode  => {
+
+    return <>1</>
+   };
+   
+   
+   const ComponentTwo = (): React.ReactNode =>  {
+   
+    return <div>2</div>;
+   
+   };
+  
+    const [currentComponent, setCurrentComponent] = useState<React.ReactNode>(null);
+  
+  
+    const value = useSelector((state: CounterState) => state.value);
+ const dispatch = useDispatch();
+
+ const handleUpdate = (key: string) => {
+   dispatch(update(key));
+ };
+  
+  
+    function click1(key:string, newComponent:any):void {
+      let test = (document.getElementById(key) as HTMLElement);
+      
+      let a = window.getComputedStyle(test);
+      if (a.backgroundColor != 'rgb(226, 245, 255)')
+      {
+
+      }
+      else
+      {
+        test.style.backgroundColor = '#5AC3F8';
+        setCurrentComponent(newComponent);
+      }
+      for (let i = 1; i < 8; i++)
+      {
+        let key_ = "NetworkD-"+i
+        if(key_ != key)
+        {
+         (document.getElementById(key_) as HTMLElement).style.backgroundColor = '#E2F5FF';
+        }
+       
+      }
+      
+      }
+      
+      useEffect(() => {
+        setCurrentComponent(currentComponent)
+        var x = document.getElementById('diagnosis_frame') as HTMLElement;
+        x.innerHTML = ReactDomServer.renderToString(currentComponent as ReactElement);
+      },[currentComponent])
+  
+
   return (
-    <div className={`${resets.storybrainResets} ${props.classes?.root || ''} ${props.className || ''} ${classes.root}`}>
-      <div className={classes.networkDiagnostics}>Network Diagnostics</div>
-      <div className={classes.vector3}>
-        <Vector3Icon className={classes.icon} />
-      </div>
-    </div>
+    <>
+    <button id = "NetworkD" className={classes.sidebar_button} onClick={() => {
+      let test = (document.getElementById("NetworkD") as HTMLElement);
+      let a = window.getComputedStyle(test);
+      if (a.background == 'rgb(13, 89, 127)')
+    {
+
+    }
+    else
+    {
+      handleUpdate('NetworkD')
+      setCurrentComponent(ComponentOne)
+    }
+    }} style={value != "NetworkD" ? {} : {background: '#0D597F', color: 'white', fontWeight: '700'} } >Network Diagnostics
+        {value != "NetworkD" ? (
+          <SlArrowRight style = {{transition: 'transform 0.15s ease-in-out'}} stroke="#c3c3c3" strokeWidth={50} color='#c3c3c3' size={16}/>
+        ) : (
+          <SlArrowRight style = {{transform: 'rotate(90deg)', transition: 'transform 0.15s ease-in-out'}}  stroke="white" strokeWidth={50} color='white' size={16}/>
+        )
+        }
+        </button>
+        {value == "NetworkD" && (
+        <div className={classes.div_bar}>
+          <button id='NetworkD-1' style={{background:'#5AC3F8'}} onClick={() => click1("NetworkD-1", ComponentOne)} className={classes.inner_sidebar_button}>•  Ping</button>
+          <button id='NetworkD-2' onClick={() => click1("NetworkD-2", ComponentTwo)}  className={classes.inner_sidebar_button}>•  Tracert</button>
+        </div>)}
+     </> 
   );
 });
